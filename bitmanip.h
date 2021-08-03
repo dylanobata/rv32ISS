a05d99afb04bc290d735bdceb393c4c88dc80e87
Dylan Obata <dylanobata@gmail.com>
2021-08-03 06:20:07 -0700

    a small bit manipulation library

--- bitmanip.h
+++ bitmanip.h
@@ -2,11 +2,9 @@
 #define BITMANIP
 
 #include <stdint.h>
-#include <stdio.h>
 
-int32_t get_bits(uint32_t bits, uint8_t start, uint8_t end) {
-    // returns bits from [start, end) 
-    return (bits >> end) & ((1 << (start-end+1))-1); 
+int32_t get_bits(uint8_t n, uint8_t position ,uint32_t bits) {
+    return (((1 << n) - 1) & (bits >> (position - 1)));
 }
 
 int32_t sign_extend(uint32_t bits, unsigned num_bits) {
@@ -21,12 +19,5 @@ void print_binary(uint32_t bits)
     for(int i=sizeof(bits) << 3; i; i--)
         putchar('0' + ((bits >> (i-1)) & 1));
     printf("\n"); 
-}
-
-
-int main() {
-    int b = 0b1010;
-    print_binary(get_bits(b,3,4));
-    return 0;
 }
 #endif
